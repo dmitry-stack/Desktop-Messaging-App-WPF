20a9f123d87ee785a8a883e5ae402b755d971696
hais dmitry <haisd@p30108.MINI>
2025-05-23 16:08:20 +0200

    new

---
+++ Wpf2/Wpf2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int index = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void AddMessage(string mes)
+        {
+            var bubble = new UserControl1(index, mes);
+            Panel.Children.Add(bubble);
+            index++;
+        }
+        public void SendMes(string text)
+        {
+            var bubble = new UserControl1(index, text)
+            {
+                Stamp = DateTime.Now
+            };
+
+            Panel.Children.Add(bubble);
+            index++;
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Simple Chat Client");
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = Input.Text.Trim();
+            if (!string.IsNullOrEmpty(text))
+            {
+                SendMes(text);
+                Input.Text = null;
+            }
+        }
+    }
+}
---
+++ Wpf2/Wpf2/UserControl1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf2
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public int index;
+        public DateTime Stamp = DateTime.Now;
+        public string Date
+        {
+            get
+            {
+                var now= DateTime.Now;
+                var diff= now - Stamp;
+                //if (diff.TotalMinutes < 1)
+                //{
+                //    return "Now";
+
+                //}
+               return Stamp.ToString("dd/MM/yyyy");
+            }
+
+        }
+        
+        public UserControl1(int index,string text)
+        {
+            InitializeComponent();
+            
+
+            if (index % 2 == 0)
+            {
+                bubbleBorder.HorizontalAlignment = HorizontalAlignment.Left;
+                usernameText.Text =Environment.UserName;
+                date.Text = Date;
+            }
+            else
+            {
+                this.HorizontalAlignment = HorizontalAlignment.Right;
+                usernameText.Text = "User";
+                date.Text = Date;
+            }
+
+            messageContent.Text = text;
+
+        }
+    }
+}
